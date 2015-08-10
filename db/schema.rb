32f8acb73c5e67c682914b5f96124f35e3448231
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-10 07:13:22 +0000

    add created at in show

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,11 @@
 ActiveRecord::Schema.define(version: 20153210063237) do
 
   create_table "messages", force: :cascade do |t|
-    t.string "author"
-    t.string "content"
-    t.string "url"
+    t.string   "author"
+    t.string   "content"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
